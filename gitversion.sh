986c30c0cafd67e17542bf0d096147727d21750f
Vishnu <vishnu.g@prosperoware.com>
2019-03-28 11:22:44 +0530

    Test commit

---
+++ gitversion.sh
@@ -0,0 +1,10 @@
+git status
+git checkout master
+git pull origin master
+git merge dev
+git push origin master
+npm version patch
+git push origin master --tags
+git checkout dev
+git merge dev
+git push origin dev
