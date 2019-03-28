becc9fd718880dd6c530ee495aaecd7f3274d13d
Vishnu <vishnu.g@prosperoware.com>
2019-03-28 14:30:52 +0530

    Changes

--- gitversion.sh
+++ gitversion.sh
@@ -1,4 +1,5 @@
-git status
+Status=`git status`
+echo $Status
 git checkout master
 git pull origin master
 git merge dev
@@ -6,5 +7,5 @@ git push origin master
 npm version patch
 git push origin master --tags
 git checkout dev
-git merge dev
+git merge master
 git push origin dev
